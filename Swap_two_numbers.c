4d216a140ae67548795a0358d95b2b2f726301c1
22A91A04K4 <119720594+22A91A04K4@users.noreply.github.com>
2022-12-09 09:54:05 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     printf("%d %d",b,a); }

--- Swap_two_numbers.c
+++ Swap_two_numbers.c
@@ -1,17 +1,8 @@
 #include<stdio.h>
-void c(int p,int q);
 int main()
 {
     int a,b;
     scanf("%d%d",&a,&b);
-    c(a,b);
-}
-void c(int p,int q)
-{
-    int c,d;
-    c=p;d=q;
-    p=q;p=q;
-    p=d;p=c;
     printf("%d
-%d",d,c);
-    }
+%d",b,a);
+}
