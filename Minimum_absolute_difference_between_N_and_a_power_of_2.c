11e5896098dc8fa3f8532c3fd68b71e8a13ec829
22A91A04K4 <119720594+22A91A04K4@users.noreply.github.com>
2022-12-28 19:55:19 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,np,sp,d,e,mp,cp;     scanf("%d",&n);     np=0;     for(i=1;np<n;i++)     {         np=pow(2,i);         if(np<=n)         {              cp=np; 		}     }     d=n-cp;     sp=n+30;     mp=sp;     while(mp>n)     {         for(i=1;pow(2,i)<=n+30;i++)         {             if(mp==pow(2,i))             {                 sp=mp;             }         }         mp--;     }     e=sp-n;     if(d<e)     {         printf("%d",d);     }     else     {         printf("%d",e);     } }

---
+++ Minimum_absolute_difference_between_N_and_a_power_of_2.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i,np,sp,d,e,mp,cp;
+    scanf("%d",&n);
+    np=0;
+    for(i=1;np<n;i++)
+    {
+        np=pow(2,i);
+        if(np<=n)
+        {
+             cp=np;
+		}
+    }
+    d=n-cp;
+    sp=n+30;
+    mp=sp;
+    while(mp>n)
+    {
+        for(i=1;pow(2,i)<=n+30;i++)
+        {
+            if(mp==pow(2,i))
+            {
+                sp=mp;
+            }
+        }
+        mp--;
+    }
+    e=sp-n;
+    if(d<e)
+    {
+        printf("%d",d);
+    }
+    else
+    {
+        printf("%d",e);
+    }
+}
