1318516128f617af7ba5905e5647137921cf0753
22A91A04K4 <119720594+22A91A04K4@users.noreply.github.com>
2023-01-05 22:17:15 +0530

    #include<stdio.h> int main() {     int a,b,c,n,r,i;     scanf("%d%d%d",&a,&r,&n);     a==n;     for(i=1;i<=r;i++)     {         c=a*i;         printf("%d x %d = %d ",a,i,c);     }
    }

---
+++ Multiplication_Table_2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,n,r,i;
+    scanf("%d%d%d",&a,&r,&n);
+    a==n;
+    for(i=1;i<=r;i++)
+    {
+        c=a*i;
+        printf("%d x %d = %d
+",a,i,c);
+    }
+    
+}
