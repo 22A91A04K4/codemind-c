b568a1476aebad0a5d3f88909ad8d2040a6a98cb
22A91A04K4 <119720594+22A91A04K4@users.noreply.github.com>
2023-01-19 18:15:23 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,c,j,k=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(a[i]==a[j])             {                 c++;                 if(i!=j)                 {                 	a[j]=0; 				}             }         }         if(a[i]==c)         {             printf("%d ",a[i]);             k++;         }     }     if(k==0)     {         printf("-1");     } }

---
+++ Elements_same_as_count.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,c,j,k=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                c++;
+                if(i!=j)
+                {
+                	a[j]=0;
+				}
+            }
+        }
+        if(a[i]==c)
+        {
+            printf("%d ",a[i]);
+            k++;
+        }
+    }
+    if(k==0)
+    {
+        printf("-1");
+    }
+}
